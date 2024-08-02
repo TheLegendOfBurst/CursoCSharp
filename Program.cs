5c4391940248e8139e3e0ac1676317b4b6084230
Burst <141189394+TheLegendOfBurst@users.noreply.github.com>
2024-08-02 17:28:22 -0300

    atividade 1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,27 @@
 ﻿using CursoCSharp;
+using CursoCSharp.Exercicios;
 
-// TiposPrimitivos.ImprimirTipos();
+//TiposPrimitivos.ImprimirTipos();
 
-EntradaSaidaDados.LerArmazenar();
+//EntradaSaidaDados.LerArmazenar();
+
+ExercicioTiposPrimitivos.Exercicio1();
+ExercicioTiposPrimitivos.Exercicio2();
+ExercicioTiposPrimitivos.Exercicio3();
+ExercicioTiposPrimitivos.Exercicio4();
+ExercicioTiposPrimitivos.Exercicio5();
+ExercicioTiposPrimitivos.Exercicio6();
+ExercicioTiposPrimitivos.Exercicio7();
+ExercicioTiposPrimitivos.Exercicio8();
+ExercicioTiposPrimitivos.Exercicio9();
+ExercicioTiposPrimitivos.Exercicio10();
+ExercicioTiposPrimitivos.Exercicio11();
+ExercicioTiposPrimitivos.Exercicio12();
+ExercicioTiposPrimitivos.Exercicio13();
+ExercicioTiposPrimitivos.Exercicio14();
+ExercicioTiposPrimitivos.Exercicio15();
+ExercicioTiposPrimitivos.Exercicio16();
+ExercicioTiposPrimitivos.Exercicio17();
+ExercicioTiposPrimitivos.Exercicio18();
+ExercicioTiposPrimitivos.Exercicio19();
+ExercicioTiposPrimitivos.Exercicio20();
--- obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33b58640f870c38b0116074bf3c0a036d7909489")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2560d820d2c753d592b74d8d34fc6e584bda485f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
