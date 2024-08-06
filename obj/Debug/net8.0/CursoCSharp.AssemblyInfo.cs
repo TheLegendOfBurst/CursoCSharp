7abb855070f17454f34e8a8472aec85c8c871f7e
TheLegendOfBurst <Gianidavi05@gmail.com>
2024-08-05 23:42:45 -0300

    test

--- obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/CursoCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8ee01f799eeaf60dab028ae4e1b8fb5465d203a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ae220495d62a7a114d3607b8ef8a54b06af2896")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
